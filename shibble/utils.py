90bbf147094a467d1d2f3da8a8b381a2532c6a84
Andy Botting <andy@andybotting.com>
2016-12-12 11:17:58 +1100

    Switch to use AAF Rapid Connect

--- shibble/utils.py
+++ shibble/utils.py
@@ -10,7 +10,7 @@ import ldap
 import ldap.modlist as modlist
 
 from shibble import cfg
-from shibble.models import update_user_state
+from shibble.models import User
 
 LOG = logging.getLogger('shibble.utils')
 CONF = cfg.CONF
@@ -106,8 +106,8 @@ def user_exists(user):
             return True
         else:
             return False
-    except ldap.LDAPError as e:
-        LOG.error('LDAP account search failed: {}'.format(e))
+    #except ldap.LDAPError as e:
+    #    LOG.error('LDAP account search failed: {}'.format(e))
     finally:
         l.unbind_s()
 
@@ -135,6 +135,8 @@ def create_user(db, shib_attrs, password):
         attrs['description'] = name
         attrs['gecos'] = name
         attrs['userPassword'] = password
+
+        LOG.warning(attrs)
         ldif = modlist.addModlist(attrs)
 
         try:
@@ -144,7 +146,37 @@ def create_user(db, shib_attrs, password):
             LOG.info("Unix account created for {}".format(mail))
             # send_signup_email(name, mail, password)
             update_user_state(db, shib_attrs, 'created')
-        except ldap.LDAPError as e:
-            LOG.exception('LDAP account creation failed: {}'.format(e))
+        #except ldap.LDAPError as e:
+        #    LOG.exception('LDAP account creation failed: {}'.format(e))
         finally:
             l.unbind_s()
+
+
+def create_db_user(db, shib_attrs):
+    """Create a new user in the DB from the Shibboleth attributes
+    Required Shibboleth attributes are `id`, `fullname` and `mail`
+    Return a newly created user.
+    """
+    # add shibboleth user
+    shibuser = User(shib_attrs["id"])
+    db.add(shibuser)
+    db.commit()
+    return shibuser
+
+
+def update_db_user(db, shib_user, shib_attrs):
+    """Update a Shibboleth User with new details passed from
+    Shibboleth.
+    """
+    shib_user.displayname = shib_attrs["fullname"]
+    shib_user.email = shib_attrs["mail"]
+    shib_user.shibboleth_attributes = shib_attrs
+    db.commit()
+
+
+def update_user_state(db, shib_attrs, state):
+    shib_user = db.query(User).filter_by(
+        persistent_id=shib_attrs["id"]).first()
+    shib_user.state = state
+    db.commit()
+
--- shibble/views.py
+++ shibble/views.py
@@ -4,14 +4,18 @@ import logging
 import json
 import functools
 
-from bottle import route, request, static_file
+from bottle import route
+from bottle import request
+from bottle import redirect
+from bottle import static_file
 from bottle import jinja2_template as template
+
 from paste.deploy.config import CONFIG
 from weberror import errormiddleware
 
+from shibble import jwt
 from shibble import utils
-from shibble.models import (User, create_shibboleth_user,
-                            update_shibboleth_user)
+from shibble import models
 
 LOG = logging.getLogger('shibble.views')
 
@@ -21,26 +25,13 @@ STATIC_FILES = path.join(path.dirname(__file__), 'static')
 template = functools.partial(template, request=request)
 
 
-class ShibbolethAttrMap(object):
-    data = {'persistent-id': 'id',
-            'cn': 'cn',
-            'displayName': 'fullname',
-            'givenName': 'firstname',
-            'sn': 'surname',
-            'uid': 'uid',
+class RapidConnectAttrMap(object):
+    data = {'cn': 'cn',
+            'displayname': 'fullname',
+            'givenname': 'firstname',
+            'surname': 'surname',
             'mail': 'mail',
-            'eppn': 'eppn',
-            'l': 'location',
-            'description': 'description',
-            'o': 'organisation',
-            'affiliation': 'affiliation',
-            'unscoped-affiliation': 'unscoped-affiliation',
-            'assurance': 'assurance',
-            'Shib-Identity-Provider': 'idp',
-            'shared-token': 'shared_token',
-            'homeOrganization': 'homeorganisation',
-            'homeOrganizationType': 'homeorganisationtype',
-            'telephoneNumber': 'telephonenumber'}
+            'edupersontargetedid': 'id'}
 
     @classmethod
     def parse(cls, environ):
@@ -48,9 +39,9 @@ class ShibbolethAttrMap(object):
         for k, v in cls.data.items():
             if environ.get(k):
                 if k == "mail":
-                    metadata[v] = environ.get(k).lower()
+                    metadata[v] = str(environ.get(k).lower())
                 else:
-                    metadata[v] = environ.get(k)
+                    metadata[v] = str(environ.get(k))
         return metadata
 
     @classmethod
@@ -65,144 +56,175 @@ def static(filepath):
     return static_file(filepath, root=STATIC_FILES)
 
 
-@route('/account_status', method='GET')
-def account_status(db):
-    session = request.environ["beaker.session"]
-    LOG.debug("The headers are: %s." % request.environ)
-    shib_user = db.query(User).filter_by(
-        persistent_id=session["user_id"]).first()
-    data = {"state": shib_user.state}
-    return json.dumps(data)
-
-
 @route('/')
-@route('/', method='POST')
 def root(db):
-    session = request.environ["beaker.session"]
-    LOG.debug("The env vars are: %s." % request.environ)
-    shib_attrs = ShibbolethAttrMap.parse(request.environ)
-    LOG.info("The AAF responded with: %s." % shib_attrs)
+  redirect(CONFIG['rapid_connect_url'])
+
+@route('/jwt', method='POST')
+def auth_callback(db):
+    session = request.environ['beaker.session']
+    try:
+        jwt_secret = CONFIG['rapid_connect_secret']
+
+        # Assertion is passed in a POST initially, then found in the
+        # session afterwards.
+        assertion = request.forms.get('assertion')
+        if not assertion:
+            assertion = session.get('assertion')
+
+        verified_jwt = jwt.decode(assertion, jwt_secret)
+        attrs = verified_jwt['https://aaf.edu.au/attributes']
+        shib_attrs = RapidConnectAttrMap.parse(attrs)
+    except jwt.ExpiredSignature:
+        data = {
+            'title': 'Sign-in Error',
+            'message': 'Security cookie has expired',
+        }
+        return template('error', **data)
 
     errors = {}
     for field in ['id', 'mail', 'fullname']:
         if field not in shib_attrs:
             errors[field] = ("Required field '%s' can't be found." %
-                             ShibbolethAttrMap.get_attr(field))
+                             RapidConnectAttrMap.get_attr(field))
 
     if errors:
-        LOG.error("The AAF IdP is not returning the required "
-                  "attributes. The following are missing: %s. "
-                  "The following are present: %s." % (", ".join(errors.keys()),
+        LOG.error('The AAF IdP is not returning the required '
+                  'attributes. The following are missing: %s. '
+                  'The following are present: %s.' % (', '.join(errors.keys()),
                                                       shib_attrs))
         error_values = errors.values()
         error_values.sort()
         data = {
-            "title": "Error",
-            "message":  "Not enough details have been received from your "
-                        "institution to allow you to log on to the cloud. "
-                        "We need your id, your e-mail and your full name."
-                        "<br />Please contact your institution and tell them "
-                        "that their \"AAF IdP\" is broken!"
-                        "<br />Copy and paste the details below into your "
-                        "email to your institution's support desk."
-                        "<br /><b>The following required fields are missing "
-                        "from the AAF service:</b>",
-            "errors": error_values}
-        return template("error", **data)
+            'title': 'Error',
+            'subject': 'Not enough details have been received from your'
+                       'institution',
+            'message': 'We need your id, your e-mail and your full name.'
+                       '<br />Please contact your institution and tell them '
+                       'that their "AAF IdP" is broken!'
+                       '<br />Copy and paste the details below into your '
+                       'email to your institution\'s support desk.',
+            'errors': error_values}
+        return template('error', **data)
 
-    if request.forms.get("csrfmiddlewaretoken", ""):
-        session["csrfmiddlewaretoken"] = \
-          request.forms.get("csrfmiddlewaretoken")
+    if request.forms.get('csrfmiddlewaretoken', ''):
+        session['csrfmiddlewaretoken'] = \
+            request.forms.get('csrfmiddlewaretoken')
         session.save()
 
-    shib_user = db.query(User).filter_by(
+
+    shib_user = db.query(models.User).filter_by(
         persistent_id=shib_attrs["id"]).first()
     if not shib_user:
-        shib_user = create_shibboleth_user(db, shib_attrs)
+        shib_user = utils.create_db_user(db, shib_attrs)
 
-    session["user_id"] = shib_attrs["id"]
+    session['user_id'] = shib_attrs['id']
+    session['assertion'] = assertion
     session.save()
 
-    if request.forms.get("agree") and shib_user.state == "new":
-
+    if request.forms.get('agree') and shib_user.state == 'new':
         password = utils.create_password()
-
         shib_user.terms = datetime.now()
-        shib_user.state = "registered"
+        shib_user.state = 'registered'
         shib_user.password = password
-        update_shibboleth_user(db, shib_user, shib_attrs)
+        utils.update_db_user(db, shib_user, shib_attrs)
         db.commit()
 
         try:
             utils.create_user(db, shib_attrs, password)
         except Exception as e:
+            LOG.exception(e)
             data = {
-                "title": "Account creation error",
-                "message": "There was an error creating your local account",
-                "errors": [str(e)],
+                'title': 'Error',
+                'subject': 'There was an error creating your local account',
+                'message': 'We encountered an unknown error while trying to'
+                           'create your local account for this service'
+                           '<br />Please contact <a href="' 
+                           ''+ CONFIG['support_url'] + '">support</a> '
+                           'to resolve this issue.',
+                'errors': [str(e)],
             }
-            return template("error", **data)
+            return template('error', **data)
 
     if not shib_user.terms:
-        data = {"title": "Terms and Conditions."}
-        return template("terms_form", **data)
+        data = {'title': 'Terms and Conditions.'}
+        return template('terms_form', **data)
 
-    if shib_user.state == "registered":
-        data = {"title": "Creating Account...",
-                "support_url": CONFIG["support_url"]}
-        return template("creating_account", **data)
+    if shib_user.state == 'registered':
+        data = {'title': 'Creating Account...',
+                'support_url': CONFIG['support_url']}
+        return template('creating_account', **data)
 
-    if shib_user.state == "created":
-
+    if shib_user.state == 'created':
         if not utils.user_exists(shib_user.email):
-            LOG.exception("Incomplete user creation error")
+            LOG.exception('Incomplete user creation error')
             data = {
-                "title": "Error",
-                "message": 'Your details could not be found on the '
+                'title': 'Error',
+                'message': 'Your details could not be found on the '
                            'central authentication server. '
                            'Thus you will <b><i>not</i></b> be able to '
                            'access the cloud! <br />Please contact <a '
-                           'href="' + CONFIG["support_url"] + '">support</a> '
+                           'href="' + CONFIG['support_url'] + '">support</a> '
                            'to resolve this issue.'
                            '<br />The error message is:',
-                "errors": ['Incomplete user creation error']}
+                'errors': ['Incomplete user creation error']}
 
-            return template("error", **data)
+            return template('error', **data)
 
-    update_shibboleth_user(db, shib_user, shib_attrs)
+    utils.update_db_user(db, shib_user, shib_attrs)
 
-    target = CONFIG["target"]
-    if "return-path" in request.query:
-        target = request.query["return-path"]
+    target = CONFIG['target']
+    if 'return-path' in request.query:
+        target = request.query['return-path']
         if not target:
-            LOG.exception("Attempt to authenticate to URL")
+            LOG.exception('Attempt to authenticate to URL')
             data = {
-                "title": "Authentication Error",
-                "message": "No target given",
+                'title': 'Error',
+                'subject': 'No target given',
+                'message': 'No redirect target was given',
             }
-            return template("error", **data)
+            return template('error', **data)
 
-    data = {"username": shib_user.email,
-            "password": shib_user.password,
-            "target": target}
-    return template("redirect", **data)
+    data = {'username': shib_user.email,
+            'password': shib_user.password,
+            'target': target}
+    return template('redirect', **data)
 
 
-@route('/terms.html')
+@route('/account_status', method='GET')
+def account_status(db):
+    session = request.environ['beaker.session']
+    state = None
+    shib_user = db.query(models.User).filter_by(
+        persistent_id=session['user_id']).first()
+
+    if shib_user:
+        state = shib_user.state
+    data = {'state': state}
+    return json.dumps(data)
+
+
+@route('/terms')
 def terms(db):
-    return template("terms.html")
+    return template('terms_form')
+
+@route('/creating')
+def terms(db):
+    data = {'title': 'Creating Account...',
+            'support_url': CONFIG['support_url']}
+    return template('creating_account', **data)
 
 
 def error_template(head_html, exception, extra):
     from shibble.wsgiapp import support_url
     data = {
-        "title": "Error",
-        "message": "An internal error has occurred and has been logged by "
-        "our system.  Our system administrators have been notified."
-        "If you have been receiving this message often then "
-        'please contact <a href="' + support_url +
-        '">support</a> for assistance.'}
-    return template("error", **data)
+        'title': 'Error',
+        'message': 'An internal error has occurred and has been logged by '
+                   'our system. Our system administrators have been notified.'
+                   'If you have been receiving this message often then '
+                   'please contact <a href="' + CONFIG['support_url'] +
+                   '">support</a> for assistance.'}
+    return template('error', **data)
 
 
 errormiddleware.error_template = error_template
